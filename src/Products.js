83d1388926049a31ccf9f12ea19aab41a5fcd468
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-11-13 11:04:28 +0530

    Main Products page layout design and Products list maping

--- src/Products.js
+++ src/Products.js
@@ -1,8 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import ProductList from "./components/ProductList"
+import Sort from "./components/Sort";
+import Filter from "./components/Filter";
 
 const Products = () => {
-  return <Wrapper></Wrapper>;
+  return <Wrapper>
+    <div className="container grid grid-filter-column">
+      <div className="fiter-section">
+        <Filter />
+      </div>
+
+      <section className="Product-view-sort">
+        <div>
+          <Sort />
+        </div>
+        <div className="main-product">
+          <ProductList />
+        </div>
+      </section>
+    </div>
+
+  </Wrapper>;
 };
 
 const Wrapper = styled.section`
